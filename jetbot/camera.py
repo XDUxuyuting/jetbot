8b3c426326de252445706b1cd6ddf7e0554f7f1b
XDUxuyuting <51498360+XDUxuyuting@users.noreply.github.com>
2019-10-15 20:41:21 +0800

    Update camera.py

--- jetbot/camera.py
+++ jetbot/camera.py
@@ -15,10 +15,10 @@ class Camera(SingletonConfigurable):
     height = traitlets.Integer(default_value=224).tag(config=True)
     fps = traitlets.Integer(default_value=21).tag(config=True)
     capture_width = traitlets.Integer(default_value=3280).tag(config=True)
-    capture_height = traitlets.Integer(default_value=2464).tag(config=True) #宽高帧率
+    capture_height = traitlets.Integer(default_value=2464).tag(config=True) #参数设置宽高帧率
 
-    def __init__(self, *args, **kwargs):
-        self.value = np.empty((self.height, self.width, 3), dtype=np.uint8)
+    def __init__(self, *args, **kwargs):#初始化
+        self.value = np.empty((self.height, self.width, 3), dtype=np.uint8)#设置成8位3通道0矩阵
         super(Camera, self).__init__(*args, **kwargs)
 
         try:
@@ -36,33 +36,33 @@ class Camera(SingletonConfigurable):
             raise RuntimeError(
                 'Could not initialize camera.  Please see error trace.')
 
-        atexit.register(self.stop)
+        atexit.register(self.stop) #注册停机函数
 
-    def _capture_frames(self):
+    def _capture_frames(self):#读每一帧图片
         while True:
-            re, image = self.cap.read()
+            re, image = self.cap.read()#读取一帧
             if re:
                 self.value = image
             else:
                 break
                 
-    def _gst_str(self):
+    def _gst_str(self):#获得图片参数
         return 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=%d, height=%d, format=(string)NV12, framerate=(fraction)%d/1 ! nvvidconv ! video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! videoconvert ! appsink' % (
                 self.capture_width, self.capture_height, self.fps, self.width, self.height)
     
-    def start(self):
+    def start(self):#打开摄像头，创建线程
         if not self.cap.isOpened():
             self.cap.open(self._gst_str(), cv2.CAP_GSTREAMER)
         if not hasattr(self, 'thread') or not self.thread.isAlive():
             self.thread = threading.Thread(target=self._capture_frames)
             self.thread.start()
 
-    def stop(self):
+    def stop(self):#释放摄像头，子线程结束前阻塞主线程
         if hasattr(self, 'cap'):
             self.cap.release()
         if hasattr(self, 'thread'):
             self.thread.join()
             
-    def restart(self):
+    def restart(self):#重启
         self.stop()
         self.start()
